cfcdae5bac6fb82ec09af37a943053fffa658076
Hemanathagiribabu <giribabuvpy@gmail.com>
2024-02-24 00:18:17 +0530

    after breeze and authorization

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/admin',function(){ 
+    if(Gate::allows('isAdmin')) {
+        return view('admin-control-panel');
+    } else {
+        return "You are not allowed to this page";
+    }
+})->middleware(['auth', 'verified'])->name('admin');
